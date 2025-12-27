51b1e79da7e427dafdaec3d2fc66e2e1fc41b335
josephlee <hijosephl31@gmail.com>
2025-12-27 11:54:19 -0500

    move constructors

--- c++/cs246prep/hankstalica/main.cc
+++ c++/cs246prep/hankstalica/main.cc
@@ -3,6 +3,7 @@
 // lvalues =
 
 #include <iostream>
+#include <utility>
 using namespace std;
 
 class Foo {
@@ -15,6 +16,30 @@ class Foo {
         size = s;
         array = new int[size] { 0 };
     }
+
+    // Copy Constructor
+    // Foo f(g) where g is also a Foo
+    // Foo f = g;
+    Foo(const Foo& rhs) {
+        cout << "Copy constructor executed here" << endl;
+        size = rhs.size;
+        array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = rhs.array[i];
+        }
+    }
+
+    // Move constructor
+    // for dealing with r-values
+    Foo(Foo&& rhs) noexcept // idk if i need to know "noexcept"
+    { // 1 & = copy constructor 2 && means move constructor
+        cout << "Move constructor executes" << endl;
+        size = rhs.size;
+        array = rhs.array;
+        rhs.array = nullptr;
+    }
+
     // Destructor
     ~Foo() {
         if (array) {
@@ -25,8 +50,41 @@ class Foo {
     Foo operator+(const Foo& rhs) {
         Foo temp(size + rhs.size);
         auto i = 0;
+        for(; i < size; i++)
+            temp[i] = array[i];
+        for(auto j = 0; j < rhs.size; i++, j++)
+            temp[i] = rhs.array[j];
+        return temp;
     }
 
+    // Assignment Operator
+    Foo& operator=(const Foo& rhs) {
+        cout << "Copy Assignmnet Operator" << endl;
+        if (this == &rhs) {
+            return *this;
+        }
+        if (array) {
+            delete [] array;
+        }
+        size = rhs.size;
+        array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = rhs.array[i];
+        }
+        return *this;
+    }
+
+    // Move assignment operator
+    Foo& operator=(Foo&& rhs) noexcept
+    {
+        cout << "Move assignment operator executes" << endl;
+        size = rhs.size;
+        array = rhs.array;
+        rhs.array = nullptr;
+
+        return *this;
+    }
 
     int& operator[](int i) {
         return array[i];
@@ -50,5 +108,17 @@ int main() {
         f[i] = 3 * i;
     }
     cout << f << endl;
+
+    //
+
+    Foo g(f);
+    cout << g << endl;
+
+    Foo h(3);
+    cout << h << endl;
+
+    h = f + g; // f + g creates an r value in which we store it in h
+    cout << h << endl; // the move operators speed things up - instead of deleting temp items, you just directly move
+
     return 0;
 }
