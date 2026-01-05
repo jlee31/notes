9e2ccf084c5a6ee5ed0817d38859f7d64aab1032
josephlee <hijosephl31@gmail.com>
2026-01-05 15:52:02 -0500

    gdb/lldb  practice

---
+++ c++/cs246prep/ch3/gdb/basic.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    int j = 3;
+    int k = 31;
+
+    j += k;
+    k = j * 2;
+    std::cout << "hi" << std::endl;
+// -g is the compiler flag for gdb
+/*
+run it with gdb ./PROGRAM_NAME
+run will run the program
+
+
+*/
+
+/*
+macOS does not support gdb, and uses lldb instead
+
+run
+next
+step
+continue
+print
+backtrace
+quit
+
+commands are the still the same but there are differences, i will just use the
+linux ssh server instead
+
+*/
+
+}
